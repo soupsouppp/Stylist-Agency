3245e4058bc2ab526ec92bf0b3d8e03cd48cf098
babyySoup <shawnazy02@gmail.com>
2022-11-02 03:02:09 -0700

    2

---
+++ Assets/Script/Alicia.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alicia : MonoBehaviour
+{
+    public bool wearingRed;
+    public bool wearingBlack;
+    public bool wearingHeels;
+
+    public bool wearingFormalTop;
+    public bool wearingFormalBot;
+    public bool wearingFormalHair;
+
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("BlackDress") || other.CompareTag("BlackSkirt") || other.CompareTag("BlackPants")
+            || other.CompareTag("BlackShirt"))
+        {
+            wearingBlack = true;
+        }
+
+        if (other.CompareTag("RedDress") || other.CompareTag("RedSkirt") || other.CompareTag("RedPants")
+            || other.CompareTag("RedShirt"))
+        {
+            wearingRed = true;
+        }
+
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
