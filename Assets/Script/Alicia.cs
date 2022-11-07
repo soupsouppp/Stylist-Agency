946260b6cdfadfc134b3d2b7ab0ddfd109792c43
babyySoup <shawnazy02@gmail.com>
2022-11-06 20:57:37 -0800

    1

--- Assets/Script/Alicia.cs
+++ Assets/Script/Alicia.cs
@@ -15,23 +15,47 @@ public class Alicia : MonoBehaviour
     public GameObject ReactionHappy;
     public GameObject ReactionUnhappy;
     public GameObject ReactionShy;
+    public GameObject ReactionQuestion;
 
-    public void OnTriggerEnter2D(Collider2D other)
+    public void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag("BlackDress") || other.CompareTag("BlackSkirt") || other.CompareTag("BlackPants")
             || other.CompareTag("BlackShirt"))
         {
             wearingBlack = true;
+
         }
 
         if (other.CompareTag("RedDress") || other.CompareTag("RedSkirt") || other.CompareTag("RedPants")
             || other.CompareTag("RedShirt"))
         {
             wearingRed = true;
+            
+        }
+
+        if (other.CompareTag("Heels"))
+        {
+            wearingHeels = true;
         }
 
     }
 
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("BlackDress") || other.CompareTag("BlackSkirt") || other.CompareTag("BlackPants")
+            || other.CompareTag("BlackShirt"))
+        {
+            wearingBlack = false;
+            
+        }
+
+        if (other.CompareTag("RedDress") || other.CompareTag("RedSkirt") || other.CompareTag("RedPants")
+            || other.CompareTag("RedShirt"))
+        {
+            wearingRed = false;
+         
+        }
+    }
 
 
     // Start is called before the first frame update
@@ -43,6 +67,14 @@ public class Alicia : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (wearingBlack || wearingRed || wearingHeels)
+        {
+            ReactionHappy.SetActive(true);
+        }
+        else
+        {
+            ReactionHappy.SetActive(false);
+        }
+
     }
 }
