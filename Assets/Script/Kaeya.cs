2e4242b94a7f9587fd470be8cba85c55c9d0ba2c
babyySoup <shawnazy02@gmail.com>
2022-11-09 03:34:10 -0800

    111

--- Assets/Script/Kaeya.cs
+++ Assets/Script/Kaeya.cs
@@ -4,15 +4,108 @@ using UnityEngine;
 
 public class Kaeya : MonoBehaviour
 {
+    public GameObject ReactionHappy;
+    public GameObject ReactionUnhappy;
+    public GameObject ReactionShy;
+    public GameObject ReactionQuestion;
+
+    public bool Like;
+    public bool Dislike;
+    public bool Question;
+    public bool Shy;
+
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("KaeyaLike"))
+        {
+            Like = true;
+        }
+
+        if (other.CompareTag("KaeyaDislike"))
+        {
+            Dislike = true;
+        }
+
+        if (other.CompareTag("KaeyaHuh"))
+        {
+            Question = true;
+        }
+
+        if (other.CompareTag("KaeyaShy"))
+        {
+            Shy = true;
+        }
+
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("KaeyaLike"))
+        {
+            Like = false;
+        }
+
+        if (other.CompareTag("KaeyaDislike"))
+        {
+            Dislike = false;
+        }
+
+        if (other.CompareTag("KaeyaHuh"))
+        {
+            Question = false;
+        }
+
+        if (other.CompareTag("KaeyaShy"))
+        {
+            Shy = false;
+        }
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Like)
+        {
+            ReactionHappy.SetActive(true);
+        }
+        else
+        {
+            ReactionHappy.SetActive(false);
+        }
+
+
+        if (Dislike)
+        {
+            ReactionUnhappy.SetActive(true);
+        }
+        else
+        {
+            ReactionUnhappy.SetActive(false);
+        }
+
+        if (Question)
+        {
+            ReactionQuestion.SetActive(true);
+        }
+        else
+        {
+            ReactionQuestion.SetActive(false);
+        }
+
+        if (Shy)
+        {
+            ReactionShy.SetActive(true);
+        }
+        else
+        {
+            ReactionShy.SetActive(false);
+        }
     }
 }
